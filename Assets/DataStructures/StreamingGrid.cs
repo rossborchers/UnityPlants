b0e9a57c2cff94e590007d872682b61425646e92
Ross Borchers <rossborchers@gmail.com>
2020-08-22 02:03:37 +0200

    Unity project with a streaming grid and some grid math stuff as a base for the plant algos

---
+++ Assets/DataStructures/StreamingGrid.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class StreamingGrid : MonoBehaviour
+{
+        public Transform Target;
+        public Camera CullCamera;
+        [Space]
+        public bool3 Collapse;
+
+        [SerializeField]
+        public int3 LoadExtents;
+        public float BlockSize;
+
+        private Dictionary<uint, int3> _validChunks;
+        private HashSet<uint> _loadingChunks; //chunks that are waiting to load (not yet existing)
+        private HashSet<uint> _unloadingChunks; //chunks that are waiting to unload (valid and exist until unload is finished, unloads should be atomic)
+
+        /// <summary>
+        /// Please ensure to call the action that signals the load is complete so the internal model can be updated.
+        /// Hash, Position, Block Size, Complete Action
+        /// </summary>
+        public event Action<uint, float3, float, Action> OnChunkLoadBegin = delegate {  };
+
+        /// <summary>
+        /// Please ensure to call the action that signals the unload is complete so the internal model can be updated.
+        /// Hash, Position, Block Size, Complete Action
+        /// </summary>
+        public event Action<uint, float3, float, Action> OnChunkUnloadBegin = delegate {  };
+
+        void Start()
+        {
+            _validChunks = new Dictionary<uint, int3>();
+            _loadingChunks = new HashSet<uint>();
+            _unloadingChunks = new HashSet<uint>();
+        }
+
+        private void Update()
+        {
+            UpdateChunks();
+        }
+
+        void UpdateChunks()
+        {
+            // Get focal point.
+            Vector3 targetPosition = Target.transform.position;
+            int3 focalChunk = GridMath.WorldToGrid(targetPosition, BlockSize);
+
+            DebugDrawChunk(GridMath.Clamp(targetPosition, BlockSize), Color.cyan);
+
+            //TODO: fix extents being treated differently (-extents is in bounds. +extents is not) and remove the +0.5 in chunk to world. Figure out a more elegant approach? maybe
+
+            Profiler.BeginSample("LevelGenerator - Queue Create Chunks");
+
+            int3 loadSize = LoadExtents * 2;
+            loadSize = new int3(Collapse.x ? 1 :loadSize.x,Collapse.y ? 1 :loadSize.y,Collapse.z ? 1 :loadSize.z);
+            float volume = loadSize.x * loadSize.y * loadSize.z;
+            float blockExtents = BlockSize / 2f;
+
+            // Create new chunks
+            for (int i = 0; i < volume; i++)
+            {
+                //still in chunk space but moved from 1d to 3d
+                int3 positiveChunkPosition = GridMath.IndexToPosition(i, loadSize.x,
+                    loadSize.y);
+
+                //subtract extents to center position on focal chunk (otherwise it starts at focalChunk and extends into positive only.
+                int3 centeredPosition = positiveChunkPosition - new int3(Collapse.x?0:LoadExtents.x,
+                                            Collapse.y?0:LoadExtents.y, Collapse.z?0:LoadExtents.z);
+
+                int3 chunkPosition = focalChunk + centeredPosition;
+
+                //Convert to world position and add extents so the blocks corner is at the position of the chunk (chunks origin is in corner, we want it in center)
+                float3 chunkWorldPosition = GridMath.GridToWorld(chunkPosition, BlockSize) +
+                                            new float3(Collapse.x?0:blockExtents, Collapse.y?0:blockExtents, Collapse.z?0:blockExtents);
+
+
+                //Dont want to generate a block outside of the cameras view
+                if(!BlockInViewFrustum(chunkWorldPosition))
+                {
+                    continue;
+                }
+
+                uint positionHash = math.hash(chunkPosition);
+                bool chunkExists = _validChunks.ContainsKey(positionHash);
+
+                //If we are unloading a chunk in a valid cell we need to cancel it before its too late!
+                if (_unloadingChunks.Contains(positionHash))
+                {
+                    _unloadingChunks.Remove(positionHash);
+                }
+
+                //Additions
+                if (chunkExists)
+                {
+                    //Draw existing chunk bounds
+                    DebugDrawChunk(chunkWorldPosition, new Color(1, 1, 1, 0.25f));
+                }
+                else if (_loadingChunks.Contains(positionHash))
+                {
+                    DebugDrawChunk(chunkWorldPosition, Color.green); //Draw green if busy loading
+                }
+                else
+                {
+                    //Load a new chunk
+                    _loadingChunks.Add(positionHash);
+                    OnChunkLoadBegin(positionHash, chunkPosition, BlockSize, () =>
+                    {
+                        _validChunks.Add(positionHash, chunkPosition);
+                        _loadingChunks.Remove(positionHash);
+                    });
+                }
+            }
+
+            Profiler.EndSample();
+            Profiler.BeginSample("LevelGenerator - Queue Remove Chunks");
+
+            //Remove out of bounds chunks. Need to iterate over all (cant check just around valid area) to ensure they are removed even if we teleport
+            if (_validChunks.Count > volume)
+            {
+                List<Tuple<uint, int3>> toRemove = new List<Tuple<uint, int3>>();
+                foreach (var chunkKVP in _validChunks)
+                {
+                    int3 pos = chunkKVP.Value;
+
+                    bool alreadyUnloading = _unloadingChunks.Contains(chunkKVP.Key);
+
+                    float3 worldPos = GridMath.GridToWorld(pos, BlockSize) + new float3(blockExtents);
+                    //Dont want to keep a block outside of the bounds or the cameras view
+                    if (!GridMath.BlockInBounds(pos, focalChunk, LoadExtents) || !BlockInViewFrustum(worldPos))
+                    {
+                        //Offset position so its centered
+                        DebugDrawChunk(worldPos, Color.red);
+                        if (!alreadyUnloading)
+                        {
+                            toRemove.Add(new Tuple<uint, int3>(chunkKVP.Key, chunkKVP.Value));
+                        }
+                    }
+                }
+
+                foreach (Tuple<uint, int3> data in toRemove)
+                {
+                    uint hash = data.Item1;
+                    int3 chunkPosition = data.Item2;
+                    _unloadingChunks.Add(hash);
+                    OnChunkUnloadBegin(hash, chunkPosition, BlockSize, () =>
+                    {
+                        _validChunks.Remove(hash); //only mark invalid after removal as removal is atomic and we want to still treat the chunk as active before its unloaded. //TODO: this true?
+                        _unloadingChunks.Remove(hash);
+                    });
+                }
+            }
+            Profiler.EndSample();
+        }
+
+        private bool BlockInViewFrustum(float3 position)
+        {
+            if (CullCamera != null)
+            {
+                return GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(CullCamera),
+                    new Bounds(position, new float3(BlockSize)));
+            }
+
+            //If no cull camera assume always visible
+            return true;
+        }
+
+        void DebugDrawChunk(Vector3 position, Color color)
+        {
+            _chunksToDraw.Enqueue(new ChunkDebugData(position, color));
+        }
+
+        private Queue<ChunkDebugData> _chunksToDraw = new Queue<ChunkDebugData>();
+
+        private struct ChunkDebugData
+        {
+            public ChunkDebugData(Vector3 position, Color color)
+            {
+                Position = position;
+                Color = color;
+            }
+            public Vector3 Position;
+            public Color Color;
+        }
+
+        private void OnDrawGizmos()
+        {
+            while (_chunksToDraw.Count > 0)
+            {
+                ChunkDebugData chunkData = _chunksToDraw.Dequeue();
+                Gizmos.color = chunkData.Color;
+                Gizmos.DrawWireCube(chunkData.Position,
+                    new Vector3(BlockSize, BlockSize,
+                        BlockSize));
+            }
+            Gizmos.color = Color.white;
+        }
+}
---
+++ Assets/Math/GridMath.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public static class GridMath
+{
+    public static int3 WorldToGrid(float3 worldPosition, float gridBlockSize)
+    {
+        return new int3(Mathf.RoundToInt(worldPosition.x / gridBlockSize),
+            Mathf.RoundToInt(worldPosition.y / gridBlockSize),
+            Mathf.RoundToInt(worldPosition.z / gridBlockSize));
+    }
+
+    public static float3 GridToWorld(int3 chunkPosition, float gridBlockSize)
+    {
+        return new float3(chunkPosition.x * gridBlockSize,
+            chunkPosition.y * gridBlockSize,
+            chunkPosition.z * gridBlockSize);
+    }
+
+    public static int PositionToIndex(int3 position, int xMax, int yMax)
+    {
+        return position.x + (position.y * xMax) + (position.z * xMax * yMax);
+    }
+
+    public static int3 IndexToPosition(int index, int xMax, int yMax)
+    {
+        int x = index % xMax;
+        int y = (index / xMax) % yMax;
+        int z = index / (xMax * yMax);
+        return new int3(x, y, z);
+    }
+
+    public static bool BlockInBounds(int3 blockPosition, int3 centralBlock, int3 extents)
+    {
+        return !(blockPosition.x < centralBlock.x - extents.x || blockPosition.x >= centralBlock.x + extents.x ||
+                 blockPosition.y < centralBlock.y -  extents.y || blockPosition.y >= centralBlock.y +  extents.y ||
+                 blockPosition.z < centralBlock.z - extents.z || blockPosition.z >= centralBlock.z + extents.z);
+    }
+
+    public static float3 Clamp(float3 worldPosition, float blockSize)
+    {
+        return new float3(
+            Mathf.RoundToInt(worldPosition.x / blockSize) * blockSize,
+            Mathf.RoundToInt(worldPosition.y / blockSize) * blockSize,
+            Mathf.RoundToInt(worldPosition.z / blockSize) * blockSize);
+    }
+}
